41f1ac37cd70f09042cb28f4c4d3197c1f46ba62
chensy123-sys <126334897+chensy123-sys@users.noreply.github.com>
2026-04-02 19:39:49 +0800

    Delete Script directory

--- Script/data_change.R
+++
@@ -1,6 +0,0 @@
-library(dplyr);library(tibble);library(tidyr);library(readr)
-my_data <- read.table("jtpa_han.tab", header = TRUE, sep = "\t") %>% 
-  select(sex, n_hs2, edu, prevearn) %>%
-  rename(L = sex, A = edu, Z = n_hs2, Y= prevearn)
-head(my_data)
-write.csv(my_data,'jpta_han.csv')
